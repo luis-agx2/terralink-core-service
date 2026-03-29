ab1d024895cec8f8ef04d94d1d429f143a9a88a8
Luis Alejandor G <luisalejandrogarciax2@gmail.com>
2026-03-29 15:42:44 -0600

    feat: initial spring boot project setup

---
+++ src/main/java/com/terralink/platform/terralink/TerralinkApplication.java
@@ -0,0 +1,13 @@
+package com.terralink.platform.terralink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerralinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerralinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/terralink/platform/terralink/TerralinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terralink.platform.terralink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerralinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
